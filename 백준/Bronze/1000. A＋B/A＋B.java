59807a8b26dd9d9038a48aa48b7e93f29b987bca
Swimming_Yang <ysw1mst@gmail.com>
2025-11-26 11:05:54 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int answer = a + b;
+		
+		System.out.println(answer);
+
+	}
+
+}
